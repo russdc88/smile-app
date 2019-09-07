b4bfb3b6c8f6dc571980c49d5f6032b5d8ebb7b2
russdc88 <russdc88@gmail.com>
2019-09-06 19:21:27 -0600

    added jumbotron component

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/Jumbotron.css"
+
+const Jumbotron = () => <div class="jumbotron jumbotron-fluid text-center">
+<div class="container">
+	<h1 class="display-4">Smile App</h1>
+	<p class="lead">This Smile App will record your smile, reminding you to smile everyday!</p>
+</div>
+</div>;
+
+export default Jumbotron
+
---
+++ src/components/KNN.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenetModule from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+class Classify extends Component {
+
+
+}
+
+export default Classify
